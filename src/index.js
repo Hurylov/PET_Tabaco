e23c2343fe964d72988909bc36c235f85c49a36f
Hurylov Dmitriy <dimarule123@gmail.com>
2023-07-14 21:08:05 +0300

    some change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+
 // import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
 
